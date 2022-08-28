d76649bfb54f5c6f5b6114b82338e45f79a52a34
May <dhehd9709@gmail.com>
2022-08-27 20:48:44 -0700

    fix the code

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req, res) {
   res.sendFile(__dirname+"/signup.html")
 });
+
 app.post("/", function(req, res) {
   const firstName = req.body.fName;
   const lastName = req.body.lName;
